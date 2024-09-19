f47320420793c4a4af0a97666d9afe2b7f6dec40
Sayuri Gamage <sayuriyashodhara@gmail.com>
2024-09-19 23:14:17 +0530

    hql f

--- HQL/src/main/java/lk/ijse/Main.java
+++ HQL/src/main/java/lk/ijse/Main.java
@@ -19,71 +19,54 @@ public class Main {
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
-       /* Query query = session.createQuery("from Customer");
-        //insert, update, delete
-        List<Customer> customers = query.list();
-        for (Customer customer: customers){
-            System.out.println(customer);
-        }*/
+     /*   //insert
+        Query query1=session.createQuery("insert into Customer(cid,name) values (?1,?2)");
+        query1.setParameter(1,2);
+        query1.setParameter(2,"wanidu");
+        query1.executeUpdate();*/
 
 
-        //unique one data from customer
-       /* Query query1 = session.createQuery("from Customer where cid = :id");
-        query1.setParameter("id", 4);*/
-
-        /*Query query1 = session.createQuery("from Customer where cid= ?1");
-        query1.setParameter(1, 4);
-        Customer customer = (Customer) query1.uniqueResult();
-        System.out.println(customer);
-*/
-
-        //load only one column
-        /*Query query2 = session.createQuery("select name,cid from Customer");
-        List<Object[]> collection = query2.getResultList();
-        for (Object[] c : collection){
-            System.out.println(c[0]);
-            System.out.println(c[1]);
-        }*/
-
-        //Insert
-        Query query1 = session.createQuery("insert into Customer(cid,name) values(?1,?2)");
-        query1.setParameter(1,6);
-        query1.setParameter(2,"Piyal");
-        query1.executeUpdate();
-        //Update
-        /*Query query2 = session.createQuery("update Customer set name=?1 where cid=?2");
-        query2.setParameter(1,"Chethika");
-        query2.setParameter(2,4);
-        query2.executeUpdate();*/
-
-        //delete
-        /*Query query = session.createQuery("delete from Customer where cid=?1");
-        query.setParameter(1,5);
+      //update
+      /*  Query query=session.createQuery("update Customer set name=?1 where cid=?2");
+        query.setParameter(1,"supuni");
+        query.setParameter(2,1);
         query.executeUpdate();*/
 
-        //
-        /*Query query3 =session.createQuery("select cid,name from Customer where cid=?1");
-        query3.setParameter(1, 1);
-        Object[] customer = (Object[]) query3.uniqueResult();
-        for(Object o : customer){
-            System.out.println(o);
-        }
-*/
+        //delete
+      /*  Query query=session.createQuery("delete Customer where cid=?1");
+        query.setParameter(1,1);
+        query.executeUpdate();*/
 
-        //
-       /* Query query4 = session.createQuery("select cid, name from Customer");
-        List<Object[]>customers = query4.list();
-        for (Object[] objects : customers) {
-            System.out.println(Arrays.toString(objects));
+    /* Query query=session.createQuery("from Customer");
+     List<Customer> customers=query.list();
+     for (Customer customer:customers){
+         System.out.println(customer);
+     }*/
+
+
+      /*  Query query=session.createQuery("select cid,name from Customer where cid=?1");
+        query.setParameter(1,1);
+        List<Object[]> customers=query.getResultList();
+        for(Object[] customer:customers){
+            System.out.println(customer[0]);
+            System.out.println(customer[1]);
         }*/
 
-        //join column
-        Query query=session.createQuery("select a.aid,c.name from Address a inner join Customer c on a.aid = c.cid");
-        List<Object[]>customers = query.list();
-        for (Object[] objects : customers) {
-            System.out.println(Arrays.toString(objects));
-        }
 
+       /* Query query=session.createQuery("select cid,name from Customer");
+       List<Object[]> customers= query.getResultList();
+      for(Object[] customer:customers){
+          System.out.println(customer[0]+","+customer[1]);
+      }*/
+
+/*
+        Query query=session.createQuery("select a.aid,c.name from Address a inner join Customer c on a.aid = c.cid");
+          List<Object[]> collector=query.list();
+          for (Object[] objects:collector){
+              System.out.println(objects[0]+","+objects[1]);
+          }
+
+*/
 
         transaction.commit();
         session.close();
